09b380915416e9720afff5bd81b332f409e56f27
Ben Wilton <ben@chromacode.co.nz>
2017-08-17 14:56:34 +1200

    initial commit
    This is a sample of how to call the ical code

---
+++ sample-ical.sql
@@ -0,0 +1,27 @@
+DECLARE
+ 
+   l_ical_event VARCHAR2(32767);
+ 
+BEGIN
+ 
+   l_ical_event := ical_event(
+      p_start_date      => TO_DATE(:P301_START_DATE, 'DD-MON-YYYY HH24:MI')
+    , p_end_date        => TO_DATE(:P301_END_DATE ,'DD-MON-YYYY HH24:MI')
+    , p_summary         => 'Test meeting summary'
+    , p_organizer_name  => 'Ben Wilton'
+    , p_organizer_email => 'ben@example.com'
+    , p_trigger         => '60M'
+    , p_uid             => rawtohex(sys_guid()) || '@example.com' --this is just a long uniqueish string
+    , p_description     => 'This the text that appears in the meeting notes'
+    , p_location        => 'Wellington, New Zealand'
+   );
+ 
+   schemaapexcmmn.brw_send_ical_email( 
+      p_to        => 'someone.else@example.com'
+    , p_from      => 'ben@example.com'
+    , p_subj      => 'Test meeting summary'
+    , p_body_html => 'This the text that appears in the email'
+    , p_body_ical => l_ical_event
+   );
+    
+END;
